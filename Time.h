b6d3a644b84ba6c3b69d45538820f07c191ad6f9
Pakapakk <132637313+Pakapakk@users.noreply.github.com>
2023-05-12 05:07:09 +0000

    Initial commit

---
+++ Time.h
@@ -0,0 +1,28 @@
+
+
+void getTime(int &h, int &m)
+{
+  cin>>h>>m;
+}
+
+int subtract(int h1, int m1, int h2, int m2)
+{
+  int h3 = h2 - h1;
+  int m3 = m2 - m1;
+  if(h3 < 0){
+    h3 += 24;
+  }
+  if(m3 < 0){
+    h3 -= 1;
+  }
+  return h3;
+}
+
+int subtract(int m1, int m2)
+{
+  int m3 = m2 - m1;
+  if(m3 < 0){
+    m3 += 60;
+  }
+  return m3;
+}
